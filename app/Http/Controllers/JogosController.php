c6ef54956a5a551273104d629a5e3bf5d44f4c31
osmarmcn <99884631+osmarmcn@users.noreply.github.com>
2024-09-18 21:51:45 -0300

    criando a model e a tabela dos jogos

--- app/Http/Controllers/JogosController.php
+++ app/Http/Controllers/JogosController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Jogo;
 use Illuminate\Http\Request;
 // dentro do controller vamos passar toda a logica
 class JogosController extends Controller
 {
     public function index(){
-        $nome = 'FiFA';
-        $id = 5;
-        return view('jogos.index', ['nome' => $nome, 'id' => $id]);
+        // $nome = 'FiFA';
+        // $id = 5;
+        $jogos= Jogo::all();
+        // dd($jogos);
+        return view('jogos.index', ['jogos' => $jogos]);
     }
 }
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,35 +10,28 @@
 <body>
     @yield('content')
 
-    {{-- <table class="table">
+    <table class="table">
   <thead>
     <tr>
       <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Nome</th>
+      <th scope="col">Categoria</th>
+      <th scope="col">Ano de Criação</th>
+      <th scope="col">valor</th>
     </tr>
   </thead>
   <tbody>
+    @foreach($jogos as $jogo)
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <th >{{$jogo->id}}</th>
+      <th>{{$jogo->nome}}</th>
+      <th>{{$jogo->categoria}}</th>
+      <th>{{$jogo->ano_criacao}}</th>
+      <th>{{$jogo->valor}}</th>
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+    @endforeach
   </tbody>
-</table> --}}
+</table>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
